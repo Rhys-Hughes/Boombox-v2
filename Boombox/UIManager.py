a06b68061c1439ef51e9485e1314620ab511bbb7
Rhys Hughes <rhysmichaelhughes1@gmail.com>
2025-08-27 23:42:22 +0100

    changed window to an instance variable to allow the main file to use the Window object properly.

--- Boombox/UIManager.py
+++ Boombox/UIManager.py
@@ -7,44 +7,41 @@ import helper
 class Window:
     def __init__(self):
         self.self = self
+        self.window = None
     
+    #create the window instance
     def initiate(self):
+        self.window = tk.Tk()
 
-        theme = helper.get_theme()
-        dimensions = helper.get_dimensions()
+    #close the window instance to make things apply
+    def close_loop(self):
+        self.window.mainloop()
 
-        #sets up the main window
-        window = tk.Tk()
+    #creates a frame within whatever window/frame is needed
+    #
+    # field -> the window/frame being added to
+    # rows -> array of weights, for example 4 rows at 25% weight each is [25, 25, 25, 25], i = row
+    # columns -> same as rows but applies to columns, eg [30, 70], i = column
 
-        self.format_window(window)
-        
-        title = self.new_title_label(window)
-        version = self.new_version_label(window)
-
-        window.mainloop()
-    
-
-
-
-    def new_frame(self, window, rows, ):
+    def new_frame(self):
         theme = helper.get_theme()
     
     #adds a title to the page
-    def new_title_label(self, window):
+    def new_title_label(self):
         theme = helper.get_theme()
 
-        label = tk.Label(window, text = "Boombox")
+        label = tk.Label(self.window, text = "Boombox")
         label.configure(activebackground = theme["highlight_colour"], bg= theme["background_colour"], bd = theme["border_width"], fg = theme["highlight_colour"], font=["title_font"])
         label.grid(row = 0, column = 0)
 
         return label
     
     #displays the version of the software
-    def new_version_label(self, window):
+    def new_version_label(self):
         theme = helper.get_theme()
         version = helper.get_version()
 
-        label = tk.Label(window, text = version)
+        label = tk.Label(self.window, text = version)
         label.configure(activebackground = theme["highlight_colour"], bg= theme["background_colour"], bd = theme["border_width"], fg = theme["text_colour"], font=["version_font"])
         label.grid(row = 0, column = 1)
 
@@ -52,16 +49,16 @@ class Window:
 
 
     #creates the basic window
-    def format_window(self, window):
+    def format_window(self):
         #sets up the main window
         theme = helper.get_theme()
         dimensions = helper.get_dimensions()
     
-        window.title("Boombox V1.0")
-        window.geometry(dimensions)
-        window.configure(background = theme["background_colour"])
-        window.iconbitmap(default=theme["logo"])
+        self.window.title("Boombox V1.0")
+        self.window.geometry(dimensions)
+        self.window.configure(background = theme["background_colour"])
+        self.window.iconbitmap(default=theme["logo"])
 
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(theme["logo"])
 
-        window.resizable(False, False)
+        self.window.resizable(False, False)
--- Boombox/main.py
+++ Boombox/main.py
@@ -6,3 +6,9 @@ if __name__ == "__main__":
     main_window = UIM.Window()
 
     main_window.initiate()
+
+    main_window.format_window()
+    
+    main_window.new_title_label()
+
+    main_window.close_loop()
